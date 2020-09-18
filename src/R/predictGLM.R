e5180407e9496233449aa2c3d27fd6ef6a835363
James Curran <j.curran@auckland.ac.nz>
2020-09-18 15:11:18 +1200

    Cleaned up Russell's new code a bit :D Changed version number. Next step CRAN

--- src/R/predictGLM.R
+++ src/R/predictGLM.R
@@ -1,12 +1,12 @@
 #' Predictions for Generalized Linear Models
-#' 
+#'
 #' An alternative to \code{\link{predictCount}} to handle Binomial as well as
 #' Poisson models
-#' 
+#'
 #' Note: The data frame, newdata, must have the same column order and data
 #' types (e.g. numeric or factor) as those used in fitting the model.
-#' 
-#' 
+#'
+#'
 #' @param object a \code{glm} object, i.e. the output from \code{\link{glm}}.
 #' @param newdata prediction data frame.
 #' @param cilevel confidence level of the interval.
@@ -20,39 +20,61 @@
 #' @keywords htest
 #' @export predictGLM
 
-predictGLM =function(object, newdata, type="link",
-                     cilevel = 0.95, quasit=FALSE,...) {
-  if (!inherits(object, "glm")) 
+predictGLM = function(object,
+                      newdata,
+                      type = "link",
+                      cilevel = 0.95,
+                      quasit = FALSE,
+                      ...) {
+  if (!inherits(object, "glm"))
     stop("First input is not a \"glm\" object")
-  if (!is.data.frame(newdata)) 
+  
+  if (!is.data.frame(newdata))
     stop("Argument \"newdata\" is not a data frame!")
-  if(family(object)$link!="log" & family(object)$link!="logit")
+  
+  if (family(object)$link != "log" & family(object)$link != "logit")
     stop("Must be a poisson or binomial model")
-  name.row <- paste("pred", 1:nrow(newdata), sep = ".")
-  name.row <- 1:nrow(newdata)
-  x <- attr(object$terms, "term.labels")
-  y <- unlist(strsplit(x, "factor\\("))
-  z <- unlist(strsplit(y, "\\)"))
-  name.col <- z
-  if (ncol(newdata) != length(name.col)) 
+  
+  name.row = paste("pred", 1:nrow(newdata), sep = ".")
+  name.row = 1:nrow(newdata)
+  
+  x = attr(object$terms, "term.labels")
+  y = unlist(strsplit(x, "factor\\("))
+  z = unlist(strsplit(y, "\\)"))
+  name.col = z
+  
+  if (ncol(newdata) != length(name.col))
     stop("Incorrect input of the new data!")
-  dimnames(newdata) <- list(name.row, name.col)
-  pred <- predict.glm(object, newdata, se.fit = TRUE, ...)
-  Expected <- pred$fit
-  percent <- 1 - (1 - cilevel)/2
-  CIquantile=qnorm(percent)
-  if(quasit&substr(family(object)$family,1,5)=="quasi") CIquantile=qt(percent,object$df.res)
-  Conf.lower <- pred$fit - CIquantile * pred$se.fit
-  Conf.upper <- pred$fit + CIquantile * pred$se.fit
-  wk <- cbind(Expected, Conf.lower, Conf.upper)
-  if(type=="response") wk <- family(object)$linkinv(wk)
-  wk.df <- as.data.frame(wk)
-  dimnames(wk.df)[[1]] <- dimnames(newdata)[[1]]
-  dimnames(wk.df)[[2]] <- c("Estimated", "Conf.lower", "Conf.upper")
-  type <- ifelse(type=="response","response","link")
-  cat("***Estimates and CIs are on the",type,"scale***\n")
-  wk.df
-  #invisible(list(frame = wk.df, fit = pred$fit, se.fit = pred$se.fit, 
-  #    df = pred$df, cilevel = cilevel))
+  
+  dimnames(newdata) = list(name.row, name.col)
+  
+  pred = predict.glm(object, newdata, se.fit = TRUE, ...)
+  
+  Expected = pred$fit
+  percent = 1 - (1 - cilevel) / 2
+  CIquantile = qnorm(percent)
+  
+  if (quasit & substr(family(object)$family, 1, 5) == "quasi")
+    CIquantile = qt(percent, object$df.res)
+  
+  Conf.lower = pred$fit - CIquantile * pred$se.fit
+  Conf.upper = pred$fit + CIquantile * pred$se.fit
+  predictions = cbind(Expected, Conf.lower, Conf.upper)
+  
+  if (type == "response")
+    predictions = family(object)$linkinv(predictions)
+  
+  predictions.df = as.data.frame(predictions)
+  dimnames(predictions.df)[[1]] = dimnames(newdata)[[1]]
+  dimnames(predictions.df)[[2]] = c("Estimated", "Conf.lower", "Conf.upper")
+  
+  type = if(type == "response"){
+            "response"}
+         else{
+             "link"
+         }
+  
+  cat("***Estimates and CIs are on the", type, "scale***\n")
+  
+  return(predictions.df)
 }
-
