547d2bf750af4b081b561220f98653efde5f588e
jmcurran <james.m.curran@gmail.com>
2023-08-22 11:00:54 +0200

    Fixed CRAN problem with package documentation. Submitted to CRAN now.

--- R/s20x-package.R
+++ R/s20x-package.R
@@ -1,13 +1,11 @@
-#' s20x: Functions for University of Auckland Course STATS 201/208 Data Analysis
-#' 
-#' A set of functions used in teaching STATS 201/208 Data Analysis at the University of Auckland. The functions are designed to make parts of R more accessible to a large undergraduate population who are mostly not statistics majors.
-#' @docType package
-#' @name s0x
-#' 
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
 #' @import stats
 #' @import graphics 
 #' @importFrom utils packageDescription
 #' @importFrom utils read.table
 #' @importFrom methods is
-#' @author Andrew Balemi, Dineika Chandra, James Curran, Brant Deppa, Mike Forster, Brendan McArdle, Chris Wild
+## usethis namespace: end
 NULL
